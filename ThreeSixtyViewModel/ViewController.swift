3922fe0f30ddd452fe132569125c470a21fff1ee
Brad Dillon <jbradforddillon@gmail.com>
2015-09-12 15:36:50 -0400

    Initial Commit

---
+++ ThreeSixtyViewModel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ThreeSixtyViewModel
+//
+//  Created by Brad Dillon on 9/12/15.
+//  Copyright © 2015 POSSIBLE Mobile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
